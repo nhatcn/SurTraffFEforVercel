303e707a8df1f09bcf6d7d024e332fa958a4f2f4
AnhTus-dey <108685458+AnhTus-dey@users.noreply.github.com>
2025-08-07 06:37:57 +0700

    Update api.py (#102)

--- backend/api/v1/api.py
+++ backend/api/v1/api.py
@@ -6,6 +6,6 @@ api_router.include_router(camera.router, prefix="/api", tags=["Cameras"])
 api_router.include_router(user.router, prefix="/api/auth", tags=["Authentication"])
 api_router.include_router(violation.router, prefix="/api/violation", tags=["Violations"])
 api_router.include_router(pothole_detection.router, prefix="/api/pothole", tags=["Pothole Detection"])
-api_router.include_router(chatbot.router, prefix="/api/query", tags=["Chatbot"])
+api_router.include_router(chatbot.router, prefix="/api", tags=["Chatbot"])
 api_router.include_router(feedback.router, prefix="/api/feedback", tags=["Feedback"])
 # api(controller) -> service->crud (repository)->BD
